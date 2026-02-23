bb0aa2ba77066cdfbdb1ff1d332845ad956f147d
XRJ4881 <javpraaco@alum.us.es>
2026-02-23 20:01:52 +0100

    index.js permite ver el about haciendo /about

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 let express = require("express");
 let cool = require("cool-ascii-faces");
-const PORT = process.env.PORT || 3000;
+let path = require("path");
 
 let app = express();
+
+app.use(express.static("public"));
+
 app.get("/cool", (req, res) => {
   res.send(cool());
-}
-);
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "about.html"));
+});
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-}
-);
+});
