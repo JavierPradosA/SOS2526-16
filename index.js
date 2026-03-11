bdfa2541782b02cbf3038119bcaef0eb7932c712
KadmonMaruki <ignaciomerino.estudiante@gmail.com>
2026-03-11 19:14:59 +0100

    salesAPI integrado

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import { fileURLToPath } from 'url';
 
 //Import apis
 import chargingAPI from "./src/back/global-ev-charging-infraestructures.js";
+import salesAPI from "./src/back/global-ev-sales.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -20,6 +21,7 @@ app.use(bodyParser.json());
 
 
 app.use("/api/v1/global-ev-charging-infrastructures", chargingAPI);
+app.use("/api/v1/global-ev-sales", salesAPI);
 
 app.get("/about", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "about.html"));
@@ -188,194 +190,6 @@ app.delete(BASE_URL_API + "/global-ev-stock-volumes/:region_country/:year", (req
     !(d.region_country.toLowerCase() === region_country.toLowerCase() &&
       Number(d.year) === Number(year))
   );
-
-  res.sendStatus(200);
-});
-
-
-// index-IMM.js
-
-// 1. Inicializar el array
-const datos = [
-  { region: 'Australia', category: 'Historical', parameter: 'EV stock share', mode: 'Cars', powertrain: 'EV', year: 2011, unit: 'Percent', value: 0.000039253245, economic_impact: 0 },
-  { region: 'Finland', category: 'Historical', parameter: 'EV stock share', mode: 'Vans', powertrain: 'EV', year: 2021, unit: 'Percent', value: 0.3, economic_impact: 0 },
-  { region: 'Finland', category: 'Historical', parameter: 'EV stock share', mode: 'Buses', powertrain: 'BEV', year: 2022, unit: 'Vehicles', value: 550, economic_impact: 154.9 },
-  { region: 'USA', category: 'Projection-STEPS', parameter: 'EV charging points', mode: 'EV', powertrain: 'Publicly available fast', year: 2021, unit: 'Charging Points', value: 22000, economic_impact: 358.64 },
-  { region: 'Netherlands', category: 'Historical', parameter: 'EV stock', mode: 'Trucks', powertrain: 'PHEV', year: 2020, unit: 'Vehicles', value: 41, economic_impact: 6.71 },
-  { region: 'Finland', category: 'Historical', parameter: 'EV stock share', mode: 'Trucks', powertrain: 'EV', year: 2023, unit: 'Percent', value: 0.119, economic_impact: 0 },
-  { region: 'Seychelles', category: 'Historical', parameter: 'EV sales', mode: 'Cars', powertrain: 'BEV', year: 2020, unit: 'Vehicles', value: 26, economic_impact: 1.07 },
-  { region: 'Germany', category: 'Historical', parameter: 'EV stock', mode: 'Vans', powertrain: 'BEV', year: 2021, unit: 'Vehicles', value: 41000, economic_impact: 2340.04 },
-  { region: 'Finland', category: 'Historical', parameter: 'EV sales', mode: 'Cars', powertrain: 'PHEV', year: 2013, unit: 'Vehicles', value: 170, economic_impact: 6.1 },
-  { region: 'Finland', category: 'Historical', parameter: 'EV sales', mode: 'Buses', powertrain: 'BEV', year: 2016, unit: 'Vehicles', value: 13, economic_impact: 3.66 }
-];
-
-let dataII = [];
-
-
-// LOAD INITIAL DATA
-app.get("/api/v1/global-ev-sales/loadInitialData", (req, res) => {
-  if (dataII.length === 0) {
-    dataII = datos.slice();
-  
-    res.status(201).json(dataII);
-  } else {
-    
-    res.sendStatus(409);
-  }
-});
-
-
-// SAMPLE
-app.get("/samples/IMM", (req, res) => {
-
-  if (dataII.length === 0) {
-    return res.send("Aún no hay datos cargados");
-  }
-
-  const regionObjetivo = "Finland";
-
-  const filtrados = dataII.filter(d =>
-    d.region.toLowerCase() === regionObjetivo.toLowerCase()
-  );
-
-  const media =
-    filtrados.reduce((acc, d) => acc + d.economic_impact, 0) /
-    filtrados.length;
-
-  res.send(`La media de impacto económico de ${regionObjetivo} es ${media}`);
-});
-
-
-// GET COLECCIÓN
-app.get("/api/v1/global-ev-sales", (req, res) => {
-
-  let resulta = dataII;
-
-  if (req.query.region) {
-    resulta = resulta.filter(d =>
-      d.region.toLowerCase() === req.query.region.toLowerCase()
-    );
-  }
-
-  if (req.query.year) {
-    resulta = resulta.filter(d =>
-      d.year == Number(req.query.year)
-    );
-  }
-
-  if (req.query.from) {
-    resulta = resulta.filter(d =>
-      d.year >= Number(req.query.from)
-    );
-  }
-
-  if (req.query.to) {
-    resulta = resulta.filter(d =>
-      d.year <= Number(req.query.to)
-    );
-  }
-
-  res.json(resulta);
-});
-
-// GET POR REGIÓN (Devuelve array con todos los registros de esa región)
-app.get("/api/v1/global-ev-sales/:region", (req, res) => {
-  const { region } = req.params;
-
-  const resultados = dataII.filter(d => 
-    d.region.toLowerCase() === region.toLowerCase()
-  );
-
-  if (resultados.length === 0) {
-    return res.sendStatus(404);
-  }
-
-  res.json(resultados);
-});
-
-// GET INDIVIDUAL
-app.get("/api/v1/global-ev-sales/:region/:year", (req, res) => {
-
-  const { region, year } = req.params;
-
-  const item = dataII.find(d =>
-    d.region.toLowerCase() === region.toLowerCase() &&
-    d.year == Number(year)
-  );
-
-  if (!item) {
-    return res.sendStatus(404);
-  }
-
-  res.json(item);
-});
-
-
-// POST
-app.post("/api/v1/global-ev-sales", (req, res) => {
-  const newItem = req.body;
-
-  if (!newItem.region || !newItem.year) {
-    return res.sendStatus(400);
-  }
-
-  const exists = dataII.find(d =>
-    d.region.toLowerCase() === newItem.region.toLowerCase() &&
-    Number(d.year) === Number(newItem.year)
-  );
-
-  if (exists) {
-    return res.sendStatus(409); // Ya existe
-  }
-
-  dataII.push(newItem);
-  res.sendStatus(201);
-});
-
-
-// POST
-app.post("/api/v1/global-ev-sales", (req, res) => {
-  const newItem = req.body;
-
-  if (!newItem.region || !newItem.year) {
-    return res.sendStatus(400);
-  }
-  const exists = dataII.find(d =>
-    d.region.toLowerCase() === newItem.region.toLowerCase() &&
-    Number(d.year) === Number(newItem.year)
-  );
-
-  if (exists) {
-    return res.sendStatus(409); // Ya existe
-  }
-
-  dataII.push(newItem);
-  res.sendStatus(201);
-});
-
-
-// DELETE COLECCIÓN
-app.delete("/api/v1/global-ev-sales", (req, res) => {
-
-  dataII = [];
-
-  res.sendStatus(200);
-});
-
-
-// DELETE INDIVIDUAL
-app.delete("/api/v1/global-ev-sales/:region/:year", (req, res) => {
-
-  const antes = dataII.length;
-
-  dataII = dataII.filter(d =>
-    !(d.region === req.params.region.toLowerCase() &&
-      d.year == Number(req.params.year))
-  );
-
-  if (dataII.length === antes) {
-    return res.sendStatus(404);
-  }
 
   res.sendStatus(200);
 });
