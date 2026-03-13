161183e45ed87f15492956465624176a934f9aa5
adrricher <adrricher@alum.us.es>
2026-03-13 23:12:45 +0100

    f06arhV1

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import { fileURLToPath } from 'url';
 //Import apis
 import chargingAPI from "./src/back/global-ev-charging-infraestructures.js";
 import salesAPI from "./src/back/global-ev-sales.js";
-import  evStockAPI  from './src/back/global-ev-stock-volumes.js';
+import evStockAPI  from './src/back/global-ev-stock-volumes.js';
 
 
 const __filename = fileURLToPath(import.meta.url);
@@ -24,176 +24,10 @@ app.use(bodyParser.json());
 
 app.use("/api/v1/global-ev-charging-infrastructures", chargingAPI);
 app.use("/api/v1/global-ev-sales", salesAPI);
+app.use("/api/v1/global-ev-stock-volumes", evStockAPI);
 
 app.get("/about", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "about.html"));
-});
-
-//Buena Práctica: URL Base
-let BASE_URL_API = "/api/v1";
-
-//TAREA F04
-const initialData = [
-  { region_country: "Brazil", year: 2024, ev_stock: 214003, macroregion_stock: 1884600, worldwide_stock: 50000000, oil_world_displacement: 58001 },
-  { region_country: "Brazil", year: 2010, ev_stock: 0, macroregion_stock: 400, worldwide_stock: 20426, oil_world_displacement: 25 },
-  { region_country: "Canada", year: 2011, ev_stock: 555, macroregion_stock: 23800, worldwide_stock: 67526, oil_world_displacement: 88 },
-  { region_country: "Mexico", year: 2012, ev_stock: 91, macroregion_stock: 4671, worldwide_stock: 190026, oil_world_displacement: 190 },
-  { region_country: "China", year: 2013, ev_stock: 32600, macroregion_stock: 111000, worldwide_stock: 390027, oil_world_displacement: 420 },
-  { region_country: "China", year: 2014, ev_stock: 85000, macroregion_stock: 208000, worldwide_stock: 710087, oil_world_displacement: 830 },
-  { region_country: "Colombia", year: 2015, ev_stock: 124, macroregion_stock: 18620, worldwide_stock: 1250810, oil_world_displacement: 1500 },
-  { region_country: "Costa Rica", year: 2016, ev_stock: 12, macroregion_stock: 37990, worldwide_stock: 2003100, oil_world_displacement: 2100 },
-  { region_country: "Denmark", year: 2017, ev_stock: 10683, macroregion_stock: 570000, worldwide_stock: 3106800, oil_world_displacement: 3500 },
-  { region_country: "Finland", year: 2018, ev_stock: 15400, macroregion_stock: 820000, worldwide_stock: 5111000, oil_world_displacement: 6000 },
-  { region_country: "Finland", year: 2019, ev_stock: 29701, macroregion_stock: 1330000, worldwide_stock: 7219000, oil_world_displacement: 8100 }
-];
-
-let data = [];
-
-app.get(BASE_URL_API + "/global-ev-stock-volumes/loadInitialData", (req, res) => {
-  if (data.length === 0) {
-    data = initialData.slice();
-    res.status(201).json(data); //.json()===.send() pero con cabeceras json. Creo que no es necesario mandar el 201, lo reconoce automáticamente
-
-  } else {
-    res.sendStatus(409);
-  }
-}
-);
-
-app.get("/samples/ARH", (req, res) => {
-  //Lógica de index-ARH.js
-
-  let targetCountry = "Finland";
-  let targetColumn = "oil_world_displacement";
-
-  let totalPerCountry = initialData.reduce((acum, fila) => {
-    if (fila.region_country === targetCountry) {
-      acum.sum += fila[targetColumn];
-      acum.count++;
-    }
-    return acum;
-  }, { sum: 0, count: 0 }
-  );
-
-  let countryAverage = totalPerCountry.sum / totalPerCountry.count;
-
-  res.send(`La media anual de barriles de crudo ahorrados por ${targetCountry} es de ${countryAverage}`)
-});
-
-
-//GET lista datos 
-app.get("/api/v1/global-ev-stock-volumes", (req, res) => {
-  res.json(data);
-}
-);
-//GET dato
-app.get("/api/v1/global-ev-stock-volumes/:region_country/:year", (req, res) => {
-  const { region_country, year } = req.params;
-  //.find recorre el array y se para en el primer elemento que cumpla la condición
-  const registro = data.find(d =>
-    d.region_country.toLowerCase() === region_country.toLowerCase() &&
-    Number(d.year) == Number(year)
-  );
-  //Si la búsqueda no devuelve nada, es que no existe nigún registro con dichos parámetros en el array
-  if (!registro) {
-    return res.sendStatus(404);
-  }
-
-  res.json(registro);
-
-}
-);
-//Método no permitido, POST sobre dato
-app.post(BASE_URL_API + "/global-ev-stock-volumes/:region_country/:year", (req, res) => {
-  res.sendStatus(405);
-});
-
-
-//POST Dato; No debe permitir realizar post sobre otro dato
-app.post(BASE_URL_API + "/global-ev-stock-volumes", (req, res) => {
-  const newRegister = req.body;
-  //Comprobamos si tiene los atributos mínimos
-  if (!newRegister.region_country || !newRegister.year) {
-    return res.sendStatus(400)
-  }
-
-  //Comprobamos si existe ya
-  const exists = data.find(
-    d => d.region_country.toLowerCase() === newRegister.region_countrycountry.toLowerCase() &&
-      Number(d.year) == Number(newRegister.year)
-  );
-
-  //Si existe 409 CONFLICT
-  if (exists) {
-    return res.sendStatus(409);
-  }
-
-  data.push(newRegister);
-  //Creo que no es necesario mandar 201, ya que lo detecta automáticamente
-  res.sendStatus(201);
-
-});
-
-//Método no permitido, PUT sobre la lista general
-app.put(BASE_URL_API + "/global-ev-stock-volumes", (req, res) => {
-  res.sendStatus(405);
-});
-
-//PUT dato; No debe permitir realizar put a la lista general
-app.put(BASE_URL_API + "/global-ev-stock-volumes/:region_country/:year", (req, res) => {
-  const { region_country, year } = req.params;
-  const updateData = req.body;
-  //Comprobamos que el identificador a actualizar corresponde con algún registro
-  if (region_country !== updateData.region_country || Number(year) !== Number(updateData.year)) {
-    return res.sendStatus(400);
-  }
-
-  //Buscar el índice del registro en el array
-  const index = data.findIndex(d =>
-    d.region_country.toLowerCase() === region_country.toLowerCase() &&
-    Number(d.year) === Number(year)
-  );
-
-  //Recorre el array y si no encuentra ningún índice que corresponde con el país y año, devuelve -1
-  if (index === -1) {
-    return res.sendStatus(404);
-  }
-
-  //
-  data[index] = updateData;
-  res.sendStatus(200);
-
-}
-);
-
-
-//DELETE lista datos
-app.delete(BASE_URL_API + "/global-ev-stock-volumes", (req, res) => {
-  data = [];
-  res.sendStatus(200);
-});
-
-//DELETE dato
-app.delete(BASE_URL_API + "/global-ev-stock-volumes/:region_country/:year", (req, res) => {
-  const { region_country, year } = req.params;
-
-  //Comprobamos si existe el recurso
-  const existe = data.find(d =>
-    d.region_country.toLowerCase() === region_country.toLowerCase() &&
-    Number(d.year) === Number(year)
-  );
-  //Si no existe 404 NOT FOUND
-  if (!existe) {
-    return res.sendStatus(404);
-  }
-
-  //Filtramos el array, quedandonos con todo excepto el registro a borrar
-  data = data.filter(d =>
-    !(d.region_country.toLowerCase() === region_country.toLowerCase() &&
-      Number(d.year) === Number(year))
-  );
-
-  res.sendStatus(200);
 });
 
 const PORT = process.env.PORT || 3000;
--- src/back/global-ev-stock-volumes.js
+++ src/back/global-ev-stock-volumes.js
@@ -6,83 +6,212 @@ let db = new dataStore();
 //Buena Práctica: URL Base
 let BASE_URL_API = "/api/v1";
 
-function evStockAPI(app){
+function evStockAPI(app) {
     const initialData = [
-  { region_country: "Brazil", year: 2024, ev_stock: 214003, macroregion_stock: 1884600, worldwide_stock: 50000000, oil_world_displacement: 58001 },
-  { region_country: "Brazil", year: 2010, ev_stock: 0, macroregion_stock: 400, worldwide_stock: 20426, oil_world_displacement: 25 },
-  { region_country: "Canada", year: 2011, ev_stock: 555, macroregion_stock: 23800, worldwide_stock: 67526, oil_world_displacement: 88 },
-  { region_country: "Mexico", year: 2012, ev_stock: 91, macroregion_stock: 4671, worldwide_stock: 190026, oil_world_displacement: 190 },
-  { region_country: "China", year: 2013, ev_stock: 32600, macroregion_stock: 111000, worldwide_stock: 390027, oil_world_displacement: 420 },
-  { region_country: "China", year: 2014, ev_stock: 85000, macroregion_stock: 208000, worldwide_stock: 710087, oil_world_displacement: 830 },
-  { region_country: "Colombia", year: 2015, ev_stock: 124, macroregion_stock: 18620, worldwide_stock: 1250810, oil_world_displacement: 1500 },
-  { region_country: "Costa Rica", year: 2016, ev_stock: 12, macroregion_stock: 37990, worldwide_stock: 2003100, oil_world_displacement: 2100 },
-  { region_country: "Denmark", year: 2017, ev_stock: 10683, macroregion_stock: 570000, worldwide_stock: 3106800, oil_world_displacement: 3500 },
-  { region_country: "Finland", year: 2018, ev_stock: 15400, macroregion_stock: 820000, worldwide_stock: 5111000, oil_world_displacement: 6000 },
-  { region_country: "Finland", year: 2019, ev_stock: 29701, macroregion_stock: 1330000, worldwide_stock: 7219000, oil_world_displacement: 8100 }
-];
+        { region_country: "Brazil", year: 2024, ev_stock: 214003, macroregion_stock: 1884600, worldwide_stock: 50000000, oil_world_displacement: 58001 },
+        { region_country: "Brazil", year: 2010, ev_stock: 0, macroregion_stock: 400, worldwide_stock: 20426, oil_world_displacement: 25 },
+        { region_country: "Canada", year: 2011, ev_stock: 555, macroregion_stock: 23800, worldwide_stock: 67526, oil_world_displacement: 88 },
+        { region_country: "Mexico", year: 2012, ev_stock: 91, macroregion_stock: 4671, worldwide_stock: 190026, oil_world_displacement: 190 },
+        { region_country: "China", year: 2013, ev_stock: 32600, macroregion_stock: 111000, worldwide_stock: 390027, oil_world_displacement: 420 },
+        { region_country: "China", year: 2014, ev_stock: 85000, macroregion_stock: 208000, worldwide_stock: 710087, oil_world_displacement: 830 },
+        { region_country: "Colombia", year: 2015, ev_stock: 124, macroregion_stock: 18620, worldwide_stock: 1250810, oil_world_displacement: 1500 },
+        { region_country: "Costa Rica", year: 2016, ev_stock: 12, macroregion_stock: 37990, worldwide_stock: 2003100, oil_world_displacement: 2100 },
+        { region_country: "Denmark", year: 2017, ev_stock: 10683, macroregion_stock: 570000, worldwide_stock: 3106800, oil_world_displacement: 3500 },
+        { region_country: "Finland", year: 2018, ev_stock: 15400, macroregion_stock: 820000, worldwide_stock: 5111000, oil_world_displacement: 6000 },
+        { region_country: "Finland", year: 2019, ev_stock: 29701, macroregion_stock: 1330000, worldwide_stock: 7219000, oil_world_displacement: 8100 }
+    ];
 
-//Load initialData
+    //Load initialData
 
 
-//Get colección
+    //Get colección
+    app.get(BASE_URL_API + "/global-ev-stock-volumes/:region_country/:year", (req, res) => {
+
+        db.find({}, { _id: 0 }, (err, result) => {
+
+            // FILTROS
+            if (req.query.region_country) {
+                result = result.filter(d =>
+                    d.region_country === req.query.region_country.toLowerCase()
+                );
+            }
+
+            if (req.query.year) {
+                result = result.filter(d =>
+                    d.year == Number(req.query.year)
+                );
+            }
+
+            if (req.query.ev_stock) {
+                result = result.filter(d =>
+                    d.ev_stock == Number(req.query.ev_stock)
+                );
+            };
+
+
+            if (req.query.macroregion_stock) {
+                result = result.filter(d =>
+                    d.macroregion_stock == Number(req.query.macroregion_stock)
+                );
+            }
+
+            if (req.query.worldwide_stock) {
+                result = result.filter(d =>
+                    d.worldwide_stock == Number(req.query.worldwide_stock)
+                );
+            }
+
+            if (req.query.oil_world_displacement) {
+                result = result.filter(d =>
+                    d.oil_world_displacement == Number(req.oil_world_displacement)
+                );
+            }
+
+            if (req.query.from) {
+                result = result.filter(d =>
+                    d.year >= Number(req.query.from)
+                );
+            }
+
+            if (req.query.to) {
+                result = result.filter(d =>
+                    d.year <= Number(req.query.to)
+                );
+            }
+
+            // PAGINACIÓN
+            let offset = Number(req.query.offset);
+            let limit = Number(req.query.limit);
+
+            if (offset) {
+                result = result.slice(offset);
+            }
+
+            if (limit) {
+                result = result.slice(0, limit);
+            }
+
+            res.json(result);
+        });
+
+
+    });
+
+
 
 
 //Get recurso
+app.get(BASE_URL_API + "/global-ev-stock-volumes/:region_country/:year", (req, res) => {
+    const { region_country, year } = req.params;
+
+    db.find({ region_country: region_country, year: Number(year) }, (err, registros) => {
+        if (err) {
+            res.sendStatus(500);
+        } else if (registros.length === 0) {
+            res.sendStatus(404, "NOT FOUND");
+        } else {
+            // Devolvemos el primer elemento encontrado (sin el _id de NeDB)
+            const result = registros[0];
+            delete result._id;
+            res.json(result);
+        }
+    });
+});
 
 
 //Post
 app.post(BASE_URL_API + "/global-ev-stock-volumes", (req, res) => {
-  let newRegister = req.body;
-  //Comprobamos si tiene los atributos mínimos
-  if (!newRegister.region_country || !newRegister.year) {
-    return res.sendStatus(400)
-  }
-  db.find({}, () =>{
-    if(){
-
+    const newRegister = req.body;
+    //Comprobamos si tiene los atributos mínimos
+    if (
+        !newRegister.region_country ||
+        !newRegister.year ||
+        newRegister.ev_stock === undefined ||
+        newRegister.macroregion_stock === undefined ||
+        newRegister.worldwide_stock === undefined ||
+        newRegister.oil_world_displacement === undefined
+    ) {
+        return res.sendStatus(400);
     }
-    db.insert(newRegister);
-    res.sendStatus(201);
-  })
+
+
+    db.find({ region_country: newRegister.region_country, year: newRegister.year }, (err, registros) => {
+        if (registros.length > 0) {
+            res.sendStatus(409);
+
+        } else {
+            db.insert(newRegister);
+            res.sendStatus(201);
+        }
+
+    })
 
 });
 
 //Post sobre dato. NO PERMITIDO
 app.post(BASE_URL_API + "/global-ev-stock-volumes/:region_country/:year", (req, res) => {
-  res.sendStatus(405);
+    res.sendStatus(405);
 });
 
 //Put 
+app.put(BASE_URL_API + "/global-ev-stock-volumes/:region_country/:year", (req, res) => {
+    const register = req.body;
+    const { region_country, year } = req.params;
+    if (
+        !register.region_country ||
+        !register.year ||
+        register.region_country.toLowerCase() !== region_country ||
+        Number(register.year) !== year
+    ) {
+        return res.sendStatus(400);
+    }
+
+    delete body._id;
+
+    db.update({ region_country: register.region_country, year: register.year }, (err, updated) => {
+        if (updated === 0) {
+            return res.sendStatus(404);
+        } else {
+            res.sendStatus(200);
+        }
+    });
+
+});
 
 //PUT sobre la lista general.NO VÁLIDO
 app.put(BASE_URL_API + "/global-ev-stock-volumes", (req, res) => {
-  res.sendStatus(405);
+    res.sendStatus(405);
 });
 
 //Delete individual
 app.delete(BASE_URL_API + "/global-ev-stock-volumes/:region_country/:year", (req, res) => {
-  const { region_country, year } = req.params;
-  
-  db.find({ region_country : region_country , year: year}, (err, registros) => {
-    if (registros.length === 0){
-        res.sendStatus(404);
-    }else{
-        db.remove({region_country: region_country, year: year}, (err, numRemoved));
-    }
-  });  
-  
+    const { region_country, year } = req.params;
+    //Comprobamos si existe ese registro
+    db.find({ region_country: region_country, year: year }, (err, registros) => {
+        if (registros.length === 0) {
+            res.sendStatus(404);
+        } else {
+            db.remove({ region_country: region_country, year: year }, (err, numRemoved) => {
+                if (err) {
+                    res.sendStatus(500);
+                } else {
+                    res.sendStatus(200);
+                }
+            }
+            );
+        }
+    });
+
 });
 
 
 //Delete colección
 app.delete(BASE_URL_API + "/global-ev-stock-volumes", (req, res) => {
- db.remove({}, { multi: true }, () => {
-    res.sendStatus(200);
-  });
+    db.remove({}, { multi: true }, () => {
+        res.sendStatus(200);
+    });
 
 });
 
 }
-export {evStockAPI};
-
-
+export { evStockAPI };
