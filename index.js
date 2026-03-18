7fbd59b025ddbe254c68166df2442708f3770eb5
XRJ4881 <javpraaco@alum.us.es>
2026-03-18 15:01:51 +0100

    pagina info

--- index.js
+++ index.js
@@ -24,15 +24,6 @@ app.use("/api/v1/global-ev-sales", salesAPI);
 evStockAPI(app);
 app.use(handler)
 
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-app.get("/about", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "about.html"));
-});
-
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
