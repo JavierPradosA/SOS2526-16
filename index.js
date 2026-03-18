a1b7a77fca38f84ebafed1e135ee57deeaeca518
adrricher <adrricher@alum.us.es>
2026-03-18 13:09:27 +0100

    SvSetupV1

--- index.js
+++ index.js
@@ -8,19 +8,22 @@ import { fileURLToPath } from 'url';
 import {evChargingInfrastructuresAPI} from "./src/back/global-ev-charging-infrastructures.js";
 import salesAPI from "./src/back/global-ev-sales.js";
 import { evStockAPI } from './src/back/global-ev-stock-volumes.js';
+import cors from 'cors';
+import {handler} from './src/front/build/handler.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 let app = express();
 
-app.use(express.static("public"));
 app.use(express.json());
-
+app.use(cors());
 
 evChargingInfrastructuresAPI(app);
 app.use("/api/v1/global-ev-sales", salesAPI);
 evStockAPI(app);
+app.use(handler)
+
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
