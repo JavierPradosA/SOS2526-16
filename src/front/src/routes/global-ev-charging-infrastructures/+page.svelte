d191c302d7d572a7d21dd5b16811dc044737e1ce
XRJ4881 <javpraaco@alum.us.es>
2026-03-20 12:13:37 +0100

    front "entero"

--- src/front/src/routes/global-ev-charging-infrastructures/+page.svelte
+++ src/front/src/routes/global-ev-charging-infrastructures/+page.svelte
@@ -1,14 +1,14 @@
 <script>
 	// @ts-ignore
     //Alamacenamiento de datos
-    let pais = "";
-	let year = "";
-	let charging_point = "";
-	let ac_slow = "";
-	let dc_fast = "";
-	let total_power_kw = "";
-    let data = $state([]);
-    let mensaje = $state("");
+    let pais = $state("");
+	let year = $state("");
+	let charging_point = $state("");
+	let ac_slow = $state("");
+	let dc_fast = $state("");
+	let total_power_kw = $state("");
+	let data = $state([]);
+	let mensaje = $state("");
     
     //Async pq no sabemos cuando nos va a dar el resultado
 	async function getData() {
@@ -79,6 +79,47 @@
 		}
 
 	}
+	async function actualizarElemento() {
+	try {
+		const res = await fetch(`http://localhost:3000/api/v1/global-ev-charging-infrastructures/${pais}/${year}`, {
+			method: 'PUT',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				country: pais,
+				year: Number(year),
+				charging_point: Number(charging_point),
+				ac_slow: Number(ac_slow),
+				dc_fast: Number(dc_fast),
+				total_power_kw: Number(total_power_kw)
+			})
+		});
+
+		if (res.status === 200) {
+			mensaje = "Elemento actualizado";
+			await getData(); // refresca tabla
+		} else if (res.status === 400) {
+			mensaje = "Datos incorrectos";
+		} else if (res.status === 404) {
+			mensaje = "Elemento no encontrado";
+		} else {
+			mensaje = "Error inesperado";
+		}
+
+	} catch (err) {
+		console.error(err);
+		mensaje = "Error de conexión";
+	}
+}
+function cargarFormulario(dato) {
+	pais = dato.country;
+	year = dato.year;
+	charging_point = dato.charging_point;
+	ac_slow = dato.ac_slow;
+	dc_fast = dato.dc_fast;
+	total_power_kw = dato.total_power_kw;
+}
 </script>
 
 <h3>This Page is for Javi's front</h3>
@@ -87,11 +128,35 @@
 <br />
 <br />
 <button style="border-radius: 10px; background-color: cornflowerblue;" onclick={getData}>Obtener datos</button>
-<ul>
+<table style="border-collapse: collapse; width: 100%;">
+	<thead>
+		<tr style="border: 1px solid black;padding: 8px;text-align: center;">
+			<th style="border: 1px solid black;padding: 8px;text-align: center; background-color: chocolate;">Pais</th>
+			<th style="border: 1px solid black;padding: 8px;text-align: center; background-color: chocolate;">Año</th>
+			<th style="border: 1px solid black;padding: 8px;text-align: center; background-color: chocolate;">charging_point</th>
+			<th style="border: 1px solid black;padding: 8px;text-align: center; background-color: chocolate;">ac_slow</th>
+			<th style="border: 1px solid black;padding: 8px;text-align: center; background-color: chocolate;">dc_fast</th>
+			<th style="border: 1px solid black;padding: 8px;text-align: center; background-color: chocolate;">total_power_kw</th>
+			<th style="border: 1px solid black;padding: 8px;text-align: center; background-color: chocolate;">Borrar fila</th>
+			<th style="border: 1px solid black;padding: 8px;text-align: center; background-color: chocolate;">Editar fila</th>
+			
+		</tr>
+	</thead>
+	<tbody>
 	{#each data as dato (dato.country + dato.year)}
-		<li>{JSON.stringify(dato)}</li>
+		<tr>
+			<td style="border: 1px solid black;padding: 8px;text-align: center;">{dato.country}</td>
+			<td style="border: 1px solid black;padding: 8px;text-align: center;">{dato.year}</td>
+			<td style="border: 1px solid black;padding: 8px;text-align: center;">{dato.charging_point}</td>
+			<td style="border: 1px solid black;padding: 8px;text-align: center;">{dato.ac_slow}</td>
+			<td style="border: 1px solid black;padding: 8px;text-align: center;">{dato.dc_fast}</td>
+			<td style="border: 1px solid black;padding: 8px;text-align: center;">{dato.total_power_kw}</td>
+			<td style="border: 1px solid black;padding: 8px;text-align: center;"><button onclick={() =>borrarElemento(dato.country, dato.year)}>Borrar fila</button></td>
+			<td style="border: 1px solid black;padding: 8px;text-align: center;"><button onclick={() => cargarFormulario(dato)}>Editar</button></td>
+		</tr>
 	{/each}
-</ul>
+	</tbody>
+</table>
 <button style="border-radius: 10px; background-color: red;" onclick={borrarColeccion}>Borrar datos</button>
 <br>
 <br>
@@ -121,6 +186,56 @@ Crear elemento
 	<input style="border-radius: 10px;" type="number" placeholder="Total Power kW" bind:value={total_power_kw} required />
 
 	<button style="border-radius: 10px; background-color: green;" type="submit">Crear</button>
+</form>
+
+<h3>Editar elemento</h3>
+
+<form onsubmit={actualizarElemento}>
+
+	<input
+		type="text"
+		placeholder="País"
+		bind:value={pais}
+		readonly
+	/>
+
+	<input
+		type="number"
+		placeholder="Año"
+		bind:value={year}
+		readonly
+	/>
+
+	<input
+		type="number"
+		placeholder="Charging Points"
+		bind:value={charging_point}
+		required
+	/>
+
+	<input
+		type="number"
+		placeholder="AC Slow"
+		bind:value={ac_slow}
+		required
+	/>
+
+	<input
+		type="number"
+		placeholder="DC Fast"
+		bind:value={dc_fast}
+		required
+	/>
+
+	<input
+		type="number"
+		placeholder="Total Power kW"
+		bind:value={total_power_kw}
+		required
+	/>
+
+	<button type="submit">Actualizar</button>
+
 </form>
 
 <p>Estado de la operación:</p>
--- src/front/src/routes/global-ev-stock-volumes/+page.svelte
+++ src/front/src/routes/global-ev-stock-volumes/+page.svelte
@@ -3,7 +3,7 @@
     let stocks = $state([]);
     //Listar todos los recursos
     async function getData(){
-        const res = await fetch ('http://localhost:3000/api/v1/glonal-ev-stock-volumes/',{
+        const res = await fetch ('http://localhost:3000/api/v1/global-ev-stock-volumes/',{
             method: 'GET'
         });
         const data = await res.json();
@@ -13,10 +13,11 @@
     }
 </script>
 <p>Global EV Stock Volumes</p>
+<button onclick={getData}>Obtener Datos</button>
 <ul>
-    {#each stocks as stock }
+    {#each stocks as stock (stock.country + stock.year) }
     <li>
-        {}
+    JSON.stringify(stock);
     </li>
     {/each}
 </ul>
