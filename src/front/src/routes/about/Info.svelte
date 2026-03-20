ab90831bc9372e12d744119e35f95bda7d087a7b
adrricher <adrricher@alum.us.es>
2026-03-20 11:02:36 +0100

    f08arhv0

---
+++ src/front/src/routes/about/Info.svelte
@@ -0,0 +1,76 @@
+<h1>SOS2526-16</h1>
+
+<h2>Equipo y fuentes de datos</h2>
+<ul>
+  <li>
+    Javier Prados Acosta - Fuente: global-ev-charging-infrastructures
+  </li>
+  <li>
+    Adrián Rico Hernández - Fuente: global-ev-stock-volumes
+  </li>
+  <li>
+    Ignacio Javier Merino Martínez - Fuente: global-ev-sales
+  </li>
+</ul>
+
+<h2>Frontends</h2>
+<ul>
+  <li>
+    <a href="">
+      Frontend Javier
+    </a>
+  </li>
+  <li>
+    <a href="">
+      Frontend Adrián
+    </a>
+  </li>
+  <li>
+    <a href="">
+      Frontend Ignacio
+    </a>
+  </li>
+</ul>
+
+<h2>APIs</h2>
+<ul>
+  <li>
+    <a href="https://sos2526-16.onrender.com/api/v1/global-ev-charging-infrastructures">
+      API global-ev-charging-infrastructures
+    </a>
+  </li>
+  <li>
+    <a href="https://sos2526-16.onrender.com/api/v1/global-ev-stock-volumes">
+      API global-ev-stock-volumes
+    </a>
+  </li>
+  <li>
+    <a href="https://sos2526-16.onrender.com/api/v1/global-ev-sales">
+      API global-ev-sales
+    </a>
+  </li>
+</ul>
+
+<h2>Documentación APIs</h2>
+<ul>
+  <li>
+    <a href="https://sos2526-16.onrender.com/api/v1/global-ev-charging-infrastructures/docs">
+      Docs global-ev-charging-infrastructures
+    </a>
+  </li>
+  <li>
+    <a href="https://sos2526-16.onrender.com/api/v1/global-ev-stock-volumes/docs">
+      Docs global-ev-stock-volumes
+    </a>
+  </li>
+  <li>
+    <a href="https://sos2526-16.onrender.com/api/v1/global-ev-sales/docs">
+      Docs global-ev-sales
+    </a>
+  </li>
+</ul>
+
+<h2>Repositorio</h2>
+<a href="https://github.com/gti-sos/SOS2526-16" target="_blank">
+  GitHub del equipo
+</a>
---
+++ src/front/src/routes/global-ev-stock-volumes/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+
+    let stocks = $state([]);
+    //Listar todos los recursos
+    async function getData(){
+        const res = await fetch ('http://localhost:3000/api/v1/glonal-ev-stock-volumes/',{
+            method: 'GET'
+        });
+        const data = await res.json();
+        stocks = data
+
+
+    }
+</script>
+<p>Global EV Stock Volumes</p>
+<ul>
+    {#each stocks as stock }
+    <li>
+        {}
+    </li>
+    {/each}
+</ul>
