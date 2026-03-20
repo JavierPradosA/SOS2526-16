9ad7d7c0f74417ebfa12a629a780d67261edf925
XRJ4881 <javpraaco@alum.us.es>
2026-03-20 10:16:42 +0100

    front javi a falta de l08 mas o menos

--- src/front/src/routes/global-ev-charging-infrastructures/+page.svelte
+++ src/front/src/routes/global-ev-charging-infrastructures/+page.svelte
@@ -3,9 +3,15 @@
     //Alamacenamiento de datos
     let pais = "";
 	let year = "";
+	let charging_point = "";
+	let ac_slow = "";
+	let dc_fast = "";
+	let total_power_kw = "";
 	let data = $state([]);
     let mensaje = $state("");
     let mensaje2 = $state("");
+	let mensaje3 = $state("");
+
     //Async pq no sabemos cuando nos va a dar el resultado
 	async function getData() {
         //
@@ -23,6 +29,8 @@
         mensaje = "Datos cargados"
 	
 	}
+
+	//Borrado de colección
     async function borrarColeccion() {
         //
 		await fetch('http://localhost:3000/api/v1/global-ev-charging-infrastructures/', {
@@ -31,6 +39,8 @@
         mensaje2 = "Datos Borrados";
 	
 	}
+
+	//Funcion de borrado de un elemento por su id
     async function borrarElemento(pais, year) {
 	try {
 		await fetch(`http://localhost:3000/api/v1/global-ev-charging-infrastructures/${pais}/${year}`, {
@@ -43,37 +53,87 @@
 		console.error("Error borrando:", err);
 	}
 }
+
+	//Funcion para crear un elemento
+	async function crearElemento() {
+	try {
+		const res = await fetch("http://localhost:3000/api/v1/global-ev-charging-infrastructures", {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			//body a recibir
+			body: JSON.stringify({
+				country: pais,
+				year: Number(year),
+				charging_point: Number(charging_point),
+				ac_slow: Number(ac_slow),
+				dc_fast: Number(dc_fast),
+				total_power_kw: Number(total_power_kw)
+			})
+		});
+
+		//Manejo de respuestas
+		if (res.status === 201) {
+			mensaje3 = "Elemento creado correctamente";
+		} else if (res.status === 400) {
+			mensaje3 = "Datos incompletos o inválidos";
+		} else if (res.status === 409) {
+			mensaje3 = "El elemento ya existe";
+		} else {
+			mensaje3 = "Error inesperado";
+		}
+
+	} catch (err) {
+		console.error("Error en POST:", err);
+		mensaje3 = "Error de conexión con el servidor";
+	}
+}
 </script>
 
 <h3>This Page is for Javi's front</h3>
-<br>
-<button onclick={LoadData}>Cargar Datos</button>
+<br />
+<button style="border-radius: 10px; background-color: aquamarine;" onclick={LoadData}>Cargar Datos</button>
 {mensaje}
-<br>
-<br>
-<button onclick={getData}>Obtener datos</button>
+<br />
+<br />
+<button style="border-radius: 10px; background-color: cornflowerblue;" onclick={getData}>Obtener datos</button>
 <ul>
-{#each data as dato (dato.country + dato.year)}
-    <li>{JSON.stringify(dato)}</li>
-{/each}
+	{#each data as dato (dato.country + dato.year)}
+		<li>{JSON.stringify(dato)}</li>
+	{/each}
 </ul>
-<button onclick={borrarColeccion}>Borrar datos</button>
+<button style="border-radius: 10px; background-color: red;" onclick={borrarColeccion}>Borrar datos</button>
 {mensaje2}
 
+<br>
+<br>
+
+Borrar colección
 <form onsubmit={() => borrarElemento(pais, year)}>
-	<input
-		type="text"
-		placeholder="País"
-		bind:value={pais}
-		required
-	/>
+	<input style="border-radius: 10px;" type="text" placeholder="País" bind:value={pais} required />
 
-	<input
-		type="number"
-		placeholder="Año"
-		bind:value={year}
-		required
-	/>
+	<input style="border-radius: 10px;" type="number" placeholder="Año" bind:value={year} required />
 
-	<button type="submit">Borrar</button>
+	<button style="border-radius: 10px; background-color: red;" type="submit">Borrar</button>
 </form>
+<br>
+<br>
+
+Crear elemento
+<form onsubmit={() => crearElemento()}>
+	<input style="border-radius: 10px;" type="text" placeholder="País" bind:value={pais} required />
+
+	<input style="border-radius: 10px;" type="number" placeholder="Año" bind:value={year} required />
+
+	<input style="border-radius: 10px;" type="number" placeholder="Charging Points" bind:value={charging_point} required />
+
+	<input style="border-radius: 10px;" type="number" placeholder="AC Slow" bind:value={ac_slow} required />
+
+	<input style="border-radius: 10px;" type="number" placeholder="DC Fast" bind:value={dc_fast} required />
+
+	<input style="border-radius: 10px;" type="number" placeholder="Total Power kW" bind:value={total_power_kw} required />
+
+	<button style="border-radius: 10px; background-color: green;" type="submit">Crear</button>
+</form>
+{mensaje3}
