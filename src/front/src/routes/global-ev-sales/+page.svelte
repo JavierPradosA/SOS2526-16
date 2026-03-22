41b87efe8257bacfb73b859a2dd9d7177463588a
KadmonMaruki <ignaciomerino.estudiante@gmail.com>
2026-03-22 18:37:40 +0100

    retocar columnas

--- src/front/src/routes/global-ev-sales/+page.svelte
+++ src/front/src/routes/global-ev-sales/+page.svelte
@@ -177,7 +177,7 @@
 			<th style="border: 1px solid #ddd; padding: 8px;">Categoría</th>
 			<th style="border: 1px solid #ddd; padding: 8px;">Parámetro</th>
 			<th style="border: 1px solid #ddd; padding: 8px;">Modo</th>
-			<th style="border: 1px solid #ddd; padding: 8px;">Motor</th>
+			<th style="border: 1px solid #ddd; padding: 8px;">Motor(Powertrain)</th>
 			<th style="border: 1px solid #ddd; padding: 8px;">Año</th>
 			<th style="border: 1px solid #ddd; padding: 8px;">Unidad</th>
 			<th style="border: 1px solid #ddd; padding: 8px;">Valor</th>
