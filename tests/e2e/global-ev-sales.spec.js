e7c29e328a5d017e71953666f925da66c3a9be38
KadmonMaruki <ignaciomerino.estudiante@gmail.com>
2026-03-27 15:48:01 +0100

    wow

--- tests/e2e/global-ev-sales.spec.js
+++ tests/e2e/global-ev-sales.spec.js
@@ -1,22 +1,18 @@
-import {test, expect} from '@playwright/test';
+// @ts-check
+import { test, expect } from '@playwright/test';
 
-let app = 'http://localhost:3000';
+let app = 'http://localhost:3000'
 
 test('has title', async ({ page }) => {
   await page.goto(app);
 
-  await page.getByRole('link', { name: 'Frontend Ignacio' }).click();
-
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Global EV Sales/);
+  await expect(page).toHaveTitle(/Global Electrical Vehicles/);
 });
 
-test('Contiene información', async ({ page }) => {
-  await page.goto(app);
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Frontend Ignacio' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: /Global EV Sales/ })).toBeVisible();
+test('tiene informacion', async ({page}) => {
+    await page.goto(app);
+    await page.getByRole('link', { name: 'Frontend Ignacio' }).click();
+    await expect(page).toHaveTitle(/global-ev-sales/);
+    
 });
