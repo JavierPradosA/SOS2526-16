2e18ec8b0873b27418beab853a63371c2bf60229
XRJ4881 <javpraaco@alum.us.es>
2026-04-09 19:39:32 +0200

    link front vue

--- src/front/src/routes/About.svelte
+++ src/front/src/routes/About.svelte
@@ -2,80 +2,66 @@
 
 <h2>Equipo y fuentes de datos</h2>
 <ul>
-  <li>
-    Javier Prados Acosta - Fuente: global-ev-charging-infrastructures
-  </li>
-  <li>
-    Adrián Rico Hernández - Fuente: global-ev-stock-volumes
-  </li>
-  <li>
-    Ignacio Javier Merino Martínez - Fuente: global-ev-sales
-  </li>
+	<li>Javier Prados Acosta - Fuente: global-ev-charging-infrastructures</li>
+	<li>Adrián Rico Hernández - Fuente: global-ev-stock-volumes</li>
+	<li>Ignacio Javier Merino Martínez - Fuente: global-ev-sales</li>
 </ul>
 
 <h2>Frontends</h2>
 <ul>
-  <li>
-    <a href="./global-ev-charging-infrastructures">
-      Frontend Javier
-    </a>
-  </li>
-  <li>
-    <a href="./global-ev-stock-volumes">
-      Frontend Adrián
-    </a>
-  </li>
-  <li>
-    <a href="./global-ev-sales">
-      Frontend Ignacio
-    </a>
-  </li>
+	<li>
+		<a href="./global-ev-charging-infrastructures"> Frontend Javier </a>
+	</li>
+	ç
+	<li>
+		<a href="./global-ev-charging-infrastructures-vue"> Frontend Javier (Vue) </a>
+	</li>
+	<li>
+		<a href="./global-ev-stock-volumes"> Frontend Adrián </a>
+	</li>
+	<li>
+		<a href="./global-ev-sales"> Frontend Ignacio </a>
+	</li>
 </ul>
 
 <h2>APIs</h2>
 <ul>
-  <li>
-    <a href="https://sos2526-16.onrender.com/api/v1/global-ev-charging-infrastructures">
-      API global-ev-charging-infrastructures
-    </a>
-  </li>
-  <li>
-    <a href="https://sos2526-16.onrender.com/api/v1/global-ev-stock-volumes">
-      API global-ev-stock-volumes
-    </a>
-  </li>
-  <li>
-    <a href="https://sos2526-16.onrender.com/api/v1/global-ev-sales">
-      API global-ev-sales
-    </a>
-  </li>
+	<li>
+		<a href="https://sos2526-16.onrender.com/api/v1/global-ev-charging-infrastructures">
+			API global-ev-charging-infrastructures
+		</a>
+	</li>
+	<li>
+		<a href="https://sos2526-16.onrender.com/api/v1/global-ev-stock-volumes">
+			API global-ev-stock-volumes
+		</a>
+	</li>
+	<li>
+		<a href="https://sos2526-16.onrender.com/api/v1/global-ev-sales"> API global-ev-sales </a>
+	</li>
 </ul>
 
 <h2>Documentación APIs</h2>
 <ul>
-  <li>
-    <a href="https://sos2526-16.onrender.com/api/v1/global-ev-charging-infrastructures/docs">
-      Docs global-ev-charging-infrastructures
-    </a>
-  </li>
-  <li>
-    <a href="https://sos2526-16.onrender.com/api/v1/global-ev-stock-volumes/docs">
-      Docs global-ev-stock-volumes
-    </a>
-  </li>
-  <li>
-    <a href="https://sos2526-16.onrender.com/api/v1/global-ev-sales/docs">
-      Docs global-ev-sales
-    </a>
-  </li>
+	<li>
+		<a href="https://sos2526-16.onrender.com/api/v1/global-ev-charging-infrastructures/docs">
+			Docs global-ev-charging-infrastructures
+		</a>
+	</li>
+	<li>
+		<a href="https://sos2526-16.onrender.com/api/v1/global-ev-stock-volumes/docs">
+			Docs global-ev-stock-volumes
+		</a>
+	</li>
+	<li>
+		<a href="https://sos2526-16.onrender.com/api/v1/global-ev-sales/docs"> Docs global-ev-sales </a>
+	</li>
 </ul>
 
 <h2>Repositorio</h2>
-<a href="https://github.com/gti-sos/SOS2526-16" target="_blank">
-  GitHub del equipo
-</a>
+<a href="https://github.com/gti-sos/SOS2526-16" target="_blank"> GitHub del equipo </a>
 
 <h2>Deploy extra</h2>
-<a href = "https://sos2526-16-production.up.railway.app/">
-  Deploy extra en railway (Javier Prados Acosta)
+<a href="https://sos2526-16-production.up.railway.app/">
+	Deploy extra en railway (Javier Prados Acosta)
 </a>
